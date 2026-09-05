a01ccc914a8f8757a3737e6247e60c0f04f60918
Aman-kumar45 <amanthakur03032005.com@gmail.com>
2026-09-05 16:38:17 +0530

    Implement Redis caching in FastAPI application and update Docker configuration

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 import os
+import redis
 
 app = FastAPI()
 
 SERVER_NAME = os.getenv("SERVER_NAME", "unknown")
+
+redis_client = redis.Redis(
+    host="redis",
+    port=6379,
+    decode_responses=True
+)
 
 
 @app.get("/")
@@ -11,4 +18,33 @@ def home():
     return {
         "message": "Hello from FastAPI",
         "server": SERVER_NAME
+    }
+
+
+@app.get("/cache/{key}")
+def get_cache(key: str):
+    value = redis_client.get(key)
+
+    if value is None:
+        return {
+            "key": key,
+            "value": None,
+            "message": "Cache MISS"
+        }
+
+    return {
+        "key": key,
+        "value": value,
+        "message": "Cache HIT"
+    }
+
+
+@app.post("/cache/{key}")
+def set_cache(key: str, value: str):
+    redis_client.set(key, value,ex=60)
+
+    return {
+        "key": key,
+        "value": value,
+        "message": "the data is stord for 60 sec"
     }
